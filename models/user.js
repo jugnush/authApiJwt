747ab4f4e0e53a93f035ae0a9fb5f6d5175b6598
Jugnu Sharma <jugnush@Jugnus-MBP.lan>
2025-02-03 22:25:03 -0600

    login and registration working

--- models/user.js
+++ models/user.js
@@ -15,7 +15,6 @@ userSchema.methods.generateAuthToken = function () {
 	const token = jwt.sign({ _id: this._id }, secret, {
 		expiresIn: "7d",
 	});
-    console.log('tokenddddddd', token);
 	return token;
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { User } = require("../models/user");
 const joi = require("joi");
 const bcrypt = require("bcrypt");
 
-router.post("/register", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
         const { error } = validate(req.body);
         if (error) return res.status(400).send(error.details[0].message);
